3e27775c6ae24a7fff452dea066b28455521cc91
Paul Yoo <paul.yoo@triggerxr.com>
2022-01-21 00:09:59 +0900

    Upload DCGAN implementation

---
+++ DCGAN/models.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, output_c):
+        super(Generator, self).__init__()
+
+        self.dimensions = [1024, 512, 256, 128]
+
+        # The paper specifices a kernel size of 5x5, however, that does not lead to an output image of 64x64
+        layers = [self._deconv_block(z_dim, self.dimensions[0], 4, 1, 0)]
+        for i in range(1, len(self.dimensions)):
+            layers.append( self._deconv_block(self.dimensions[i - 1], self.dimensions[i], 4,  2,  1) )
+        
+        layers += [nn.Sequential(nn.ConvTranspose2d(self.dimensions[-1], output_c, 4, 2, 1),
+                                 nn.Tanh())]
+
+        self.layers = nn.ModuleList(layers)
+
+    def _deconv_block(self, in_c, out_c, k_size, stride, pad):
+        return nn.Sequential(nn.ConvTranspose2d(in_c, out_c, k_size, stride, pad, bias=False), # no need to add bias due to BatchNorm right afterwards
+                             nn.BatchNorm2d(out_c),
+                             nn.ReLU())
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(self, input_c):
+        super(Discriminator, self).__init__()
+
+        self.dimensions = [8, 16, 32, 64]
+
+        layers = [nn.Sequential(nn.Conv2d(input_c, self.dimensions[0], 4, 2, 1),
+                                nn.LeakyReLU(0.2))]
+
+        for i in range(1, len(self.dimensions)):
+            layers.append( self._conv_block(self.dimensions[i - 1], self.dimensions[i], 4,  2,  1) )
+        
+        layers += [nn.Sequential(nn.Conv2d(self.dimensions[-1], 1, 4, 2, 0),
+                                 nn.Sigmoid())]
+
+        self.layers = nn.ModuleList(layers)
+
+    def _conv_block(self, in_c, out_c, k_size, stride, pad):
+        return nn.Sequential(nn.Conv2d(in_c, out_c, k_size, stride, pad, bias=False),
+                             nn.BatchNorm2d(out_c),
+                             nn.LeakyReLU(0.2))
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x.reshape(-1, 1) # output size is N x 1
---
+++ DCGAN/train.py
@@ -0,0 +1,100 @@
+EPOCHS = 3
+BATCH_SIZE = 128
+MODEL_DATA = "DCGAN_model.pt"
+
+lr = 2e-4
+z_dim = 100
+image_channels = 1
+image_size = 64
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print('Using device', device)
+
+preprocess = transforms.Compose([
+    transforms.Resize((image_size, image_size)),
+    transforms.ToTensor(), #  normalizes image pixel values to [0, 1]
+    transforms.Normalize(mean=[0.5], std=[0.5]) #mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+train_dataset = datasets.CelebA('.', transform=preprocess, download=True)
+train = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+generator = Generator(z_dim, image_channels).to(device)
+discriminator = Discriminator(image_channels).to(device)
+
+# Initialize model weights from a normal distribution
+generator.apply(init_weights)
+discriminator.apply(init_weights)
+
+optim_g = torch.optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
+optim_d = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
+step = 0
+
+if os.path.isfile(MODEL_DATA):
+    checkpoint = torch.load(MODEL_DATA)
+    generator.load_state_dict(checkpoint['g_model_state_dict'])
+    discriminator.load_state_dict(checkpoint['d_model_state_dict'])
+
+    optim_g.load_state_dict(checkpoint['g_optimizer_state_dict'])
+    optim_d.load_state_dict(checkpoint['d_optimizer_state_dict'])
+    step = checkpoint['step']
+    d_loss = checkpoint['d_loss']
+    g_loss = checkpoint['g_loss']
+    print('Previous step:', step)
+    print('Previous generator loss', g_loss)
+    print('Previous discriminator loss', d_loss)
+
+    model.train()
+
+
+for epoch in range(EPOCHS):
+    for idx, (real_samples, _) in enumerate(train):
+        real_samples = real_samples.to(device)
+
+        z = torch.rand((BATCH_SIZE, z_dim, 1, 1)).to(device)
+        fake_samples = generator(z)
+        
+        preds_f = discriminator(fake_samples)
+        preds_r = discriminator(real_samples)
+
+        # Discriminator objective:
+        # Maximize log probability of samples from the real distribution
+        # Minimize log probability of samples from the fake distribution
+        optim_d.zero_grad()
+        discriminator_loss = F.binary_cross_entropy(preds_r, torch.ones_like(preds_r)) + F.binary_cross_entropy(preds_f, torch.zeros_like(preds_f))
+        discriminator_loss.backward(retain_graph=True) # need to retain graph in order to backpropogate through preds_f again later 
+        
+        optim_d.step()
+
+        # Generator objective:
+        # Maximize log probability of samples from the fake distribution
+        optim_g.zero_grad()
+        preds_f = discriminator(fake_samples)
+        generator_loss = F.binary_cross_entropy(preds_f, torch.ones_like(preds_f))
+        generator_loss.backward()
+
+        optim_g.step()
+
+        step += 1
+        # Print loss and save model every 30 iterations
+        if (step - 1) % 70 == 0:
+            print()
+            print('Step:', step)
+            print('Generator Loss:', generator_loss.item())
+            print('Discriminator Loss:', discriminator_loss.item())
+            
+            torch.save({'step': step,
+                        'g_model_state_dict': generator.state_dict(),
+                        'g_optimizer_state_dict': optim_g.state_dict(),
+                        'd_model_state_dict': discriminator.state_dict(),
+                        'd_optimizer_state_dict': optim_d.state_dict(),
+                        'g_loss': generator_loss.item(),
+                        'd_loss': discriminator_loss.item(),
+                        }, MODEL_DATA)
+            
+
+        
+
+
